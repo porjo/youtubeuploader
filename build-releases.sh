1442f5eff7060633accda45034ba46e3c3626a5d
Ian Bishop <ianbishop@pace7.com>
2018-03-25 07:30:37 +1000

    Add -v version flag

--- build-releases.sh
+++ build-releases.sh
@@ -2,10 +2,31 @@
 
 > sha256-checksums
 
-(env GOOS=linux GOARCH=arm GOARM=7 go build -o youtubeuploader_linux_armv7; gzip youtubeuploader_linux_armv7; sha256sum youtubeuploader_linux_armv7.gz >> sha256-checksums) &
-(env GOOS=linux GOARCH=arm64 go build -o youtubeuploader_linux_arm64; gzip youtubeuploader_linux_arm64; sha256sum youtubeuploader_linux_arm64.gz >> sha256-checksums) &
-(env GOOS=linux GOARCH=amd64 go build -o youtubeuploader_linux_amd64; gzip youtubeuploader_linux_amd64; sha256sum youtubeuploader_linux_amd64.gz >> sha256-checksums) &
-(env GOOS=windows GOARCH=amd64 go build -o youtubeuploader_windows_amd64.exe; zip youtubeuploader_windows_amd64.zip youtubeuploader_windows_amd64.exe; rm -f youtubeuploader_windows_amd64.exe; sha256sum youtubeuploader_windows_amd64.zip >> sha256-checksums) &
-(env GOOS=darwin GOARCH=amd64 go build -o youtubeuploader_mac_amd64; gzip youtubeuploader_mac_amd64; sha256sum youtubeuploader_mac_amd64.gz >> sha256-checksums) &
+(env GOOS=linux GOARCH=arm GOARM=7 go build -ldflags "-X main.appVersion=$(git describe --tags)" -o youtubeuploader_linux_armv7
+gzip youtubeuploader_linux_armv7
+sha256sum youtubeuploader_linux_armv7.gz >> sha256-checksums
+) &
+
+(env GOOS=linux GOARCH=arm64 go build -ldflags "-X main.appVersion=$(git describe --tags)" -o youtubeuploader_linux_arm64
+gzip youtubeuploader_linux_arm64
+sha256sum youtubeuploader_linux_arm64.gz >> sha256-checksums
+) &
+
+(env GOOS=linux GOARCH=amd64 go build -ldflags "-X main.appVersion=$(git describe --tags)" -o youtubeuploader_linux_amd64
+gzip youtubeuploader_linux_amd64
+sha256sum youtubeuploader_linux_amd64.gz >> sha256-checksums
+) &
+
+(env GOOS=windows GOARCH=amd64 go build -ldflags "-X main.appVersion=$(git describe --tags)" -o youtubeuploader_windows_amd64.exe
+zip youtubeuploader_windows_amd64.zip youtubeuploader_windows_amd64.exe
+rm -f youtubeuploader_windows_amd64.exe
+sha256sum youtubeuploader_windows_amd64.zip >> sha256-checksums
+) &
+
+(env GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.appVersion=$(git describe --tags)" -o youtubeuploader_mac_amd64
+zip youtubeuploader_mac_amd64.zip youtubeuploader_mac_amd64
+rm -f youtubeuploader_mac_amd64
+sha256sum youtubeuploader_mac_amd64.zip >> sha256-checksums
+) &
 
 wait
