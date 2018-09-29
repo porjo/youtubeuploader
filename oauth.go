a1f15f5c3d1cdb8310a270d87a49643f5ee44e7b
Ian Bishop <cvs.spam@pace7.com>
2018-09-29 11:29:21 +1000

    Update vendor libraries

--- oauth.go
+++ oauth.go
@@ -15,6 +15,7 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"flag"
@@ -28,7 +29,6 @@ import (
 	"runtime"
 	"time"
 
-	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 )
 
