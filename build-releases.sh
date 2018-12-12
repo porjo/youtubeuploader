c37b5e8503f629a05f450c521e9233d65cb1bcd3
Ian Bishop <1296987+porjo@users.noreply.github.com>
2018-12-12 19:24:26 +1000

    tweak build script

--- build-releases.sh
+++ build-releases.sh
@@ -6,16 +6,19 @@ VER=$(git describe --tags)
 
 (env GOOS=linux GOARCH=arm GOARM=7 go build -ldflags "-X main.appVersion=$VER" -o youtubeuploader_linux_armv7
 tar -czf youtubeuploader_linux_armv7.tar.gz  youtubeuploader_linux_armv7
+rm -f youtubeuploader_linux_armv7
 sha256sum youtubeuploader_linux_armv7.tar.gz >> sha256-checksums
 ) &
 
 (env GOOS=linux GOARCH=arm64 go build -ldflags "-X main.appVersion=$VER" -o youtubeuploader_linux_arm64
 tar -czf youtubeuploader_linux_arm64.tar.gz youtubeuploader_linux_arm64
+rm -f youtubeuploader_linux_arm64
 sha256sum youtubeuploader_linux_arm64.tar.gz >> sha256-checksums
 ) &
 
 (env GOOS=linux GOARCH=amd64 go build -ldflags "-X main.appVersion=$VER" -o youtubeuploader_linux_amd64
 tar -czf youtubeuploader_linux_amd64.tar.gz youtubeuploader_linux_amd64
+rm -f youtubeuploader_linux_amd64
 sha256sum youtubeuploader_linux_amd64.tar.gz >> sha256-checksums
 ) &
 
